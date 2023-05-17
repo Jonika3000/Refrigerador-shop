361659d96a6d439e4c7effbc3f77faa2a1de6027
Jonika <66825034+Jonika3000@users.noreply.github.com>
2023-05-17 22:00:50 +0300

    Start comment

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,7 @@
-const BASE_URL: string = process.env.REACT_APP_BASE_URL as string 
-
+const BASE_URL: string = process.env.REACT_APP_BASE_URL as string /* силлка в вишляді строки яка веде на апі  */
+/* змінна для використання замість axios в коді */
 const APP_ENV = {
     BASE_URL: BASE_URL
 };
-
+/* експорт */
 export { APP_ENV }; 
